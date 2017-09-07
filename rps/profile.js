3a3ff8ed8d551f03733fefb655919ab754cc1a89
Joanna Niklus <joanna.niklus@hotmail.com>
2017-09-07 19:39:04 +0300

    Update

--- rps/profile.js
+++ rps/profile.js
@@ -1,50 +1,64 @@
-function loadProfiles() { // loads the profiles into the dd after the page has loaded..?
-	document.getElementById("profile-select").innerHTML = dropdownOptions(getProfiles());
-
+function getData() {
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', 'profileQuery.json', true);
+	xobj.onreadystatechange = function () {
+		if (xobj.readyState == 4 && xobj.status == "200") {
+		// .open will NOT return a value but simply returns undefined in async mode so use a callback
+		var listOfProfiles = listify(xobj.responseText);
+		document.getElementById("profile-select").innerHTML = dropdownOptions(listOfProfiles);
+		}
+	}
+	xobj.send(null);
 }
 
-function getProfiles() { // supplies the list of profiles from the DB
+function listify(jsonresponse) {
 	var result = [];
-
-	// ...
-
-	return ["Username1", "Username2", "Username3"]
+	var response = JSON.parse(jsonresponse);
+	for (profile of response) {
+		result.push([profile.id, profile.username]);
+	}
+	return result;
 }
 
-function getSessions(profileName) { // supplies the list of sessions for the profile from the DB
-	var result = [];
-
-	return [[profileName, 4, 1, 6, "lastPlayed"], [profileName, "wins", "losses", "ties", "lastPlayed"], [profileName, "wins", "losses", "ties", "lastPlayed"]];
+function getSessions(profileValues) { // supplies the list of sessions for the profile from the DB
+	profileValues = profileValues.split(",");
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', 'profileId'+profileValues[0]+'.json', true);
+	xobj.onreadystatechange = function () {
+		if (xobj.readyState == 4 && xobj.status == "200") {
+			printSessions(JSON.parse(xobj.responseText), profileValues[1]);
+		}
+	}
+	xobj.send(null);	
 }
 
-function dropdownOptions(listOfProfiles) {
+function printSessions(response, userName) {
+	var i = 1;
+	var result = "";
+	for (session of response) {
+		result += "<tr><td id='" + i + "session-id'>" + i +"</td>";
+		result += "<td id='" + i + "session-username'>" + userName + "</td>";
+		session = Object.values(session);
+		
+		result += "<td id='" + i + "session-wins'>" + session[0] + "</td>";
+		result += "<td id='" + i + "session-losses'>" + session[1] + "</td>";
+		result += "<td id='" + i + "session-ties'>" + session[2] + "</td>";
+		result += "<td id='" + i + "session-time'>" + session[3] + "</td></tr>";
+		
+		i += 1;
+	};
+
+	var headerOfTable = "<tr><th>Session ID</th><th>Username</th><th>Wins</th><th>Losses</th><th>Ties</th><th>Last played</th></tr>";
+	document.getElementById("session-info").innerHTML = headerOfTable + result;
+}
+
+function dropdownOptions(listOfProfiles) { // [[id, name], [id, name], []]
 	var result = "<option disabled selected value> -- select a profile -- </option>";
 	for (profile of listOfProfiles) {
-		result += "<option>" + profile + "</option>";
+		result += "<option value=" + [profile[0],profile[1]] + ">" + profile[1] + "</option>";
 	};
 
 	return result
-}
-
-function updateTable(profileName) { // updates the table, after the selection of an option
-	console.log(profileName);
-	var headerOfTable = "<tr><th>Session ID</th><th>Username</th><th>Wins</th><th>Losses</th><th>Ties</th><th>Last played</th></tr>";	
-	var otherRowsOfTheTable = getInfoOfUser(profileName);
-	document.getElementById("session-info").innerHTML = headerOfTable + otherRowsOfTheTable;
-}
-
-function getInfoOfUser(profileName) {
-	var result = "<tr>";
-
-	var sessionsOfProfile = getSessions(profileName);
-	var i = 1;
-		for (session of sessionsOfProfile) {
-			result += "<td>" + i + "</td>";
-			for (variable of session) {
-				result += "<td>" + variable + "</td>";
-			}
-			result += "</tr>"
-			i += 1;
-		}
-	return result;
 }
